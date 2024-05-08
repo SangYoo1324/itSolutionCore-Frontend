aa724fb4fcbdbd52cf60bacc273592784565cac3
sam <samuel13241@gmail.com>
2024-05-08 00:06:21 -0700

    amplify start

--- src/_config/Env.js
+++ src/_config/Env.js
@@ -1,5 +1,5 @@
 const dev_url ="http://localhost:8080"
-const prod_url = "http://ec2-44-242-115-211.us-west-2.compute.amazonaws.com";
+const prod_url = "https://itsolution.ps-its.com";
 
 export const env = {
     api_path: prod_url
