0f1441627dde1a00232cc74e0d70a3f9f9d666bb
sam <samuel13241@gmail.com>
2024-05-08 16:48:17 -0700

    no message

--- src/_config/Env.js
+++ src/_config/Env.js
@@ -1,6 +1,6 @@
 const dev_url ="http://localhost:8080"
-const prod_url = "https://itsolution.ps-its.com";
-
+const prod_url = "https://ps-its.com";
+const http_url = "http://ec2-44-242-115-211.us-west-2.compute.amazonaws.com"
 export const env = {
     api_path: prod_url
 };
